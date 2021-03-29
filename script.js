242b1ec8158b8cc32af6ec8e92acfaf28880d39e
somaratne <erangasomaratne@gmail.com>
2021-03-29 21:36:28 +0530

    created infinte scroll

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const ACCESS_KEY = 'T3dSLCK9o7sJpAqnnQGnjW6UH59BCTmuvTVigFL6asQ';
-const count = 10;
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${ACCESS_KEY}&count=${count}`;
 const imageContainer = document.getElementById('image-container');
 
 function setPhotos(photos){
@@ -14,14 +12,23 @@ function setPhotos(photos){
 }
 
 async function getPhotos(){
+    let count = 10;
+    const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${ACCESS_KEY}&count=${count}`;
     try {
         const response = await fetch(apiUrl);
         const result = await response.json();
         const photosArray = result;
+        count = 0;
         setPhotos(photosArray) ;
     } catch (err) {
         console.error(err.message);
     }    
 }
+
+window.addEventListener('scroll',() => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
+        getPhotos();
+    }
+})
 
 getPhotos();
